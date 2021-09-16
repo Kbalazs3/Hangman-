4b13421fb1ec53b765f58a56230a2ea03cf68668
Kbalazs3 <kbalazs93@gmail.com>
2021-09-16 19:26:13 +0200

    word red from file and random word returned in function

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,5 @@
+import random
+import time
 
 
 def ask_player_name():
@@ -5,8 +7,20 @@ def ask_player_name():
     return player_name
 
 
-def main():
+def get_word():
+    with open("words.txt", 'r') as words:
+        words_red = words.read()
+        words_list = words_red.split(" ")
+        return words_list[random.randint(0, len(words_list) -1)]
+
+
+def print_result(winner):
+    print(winner + "You have won!")
+
+
+def main_game_logic():
     pass
 
 
 if __name__ == '__main__':
+    print(get_word())
