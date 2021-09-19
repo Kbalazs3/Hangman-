bb43c97e8ac687678b7693daffa1b332595f9582
Kbalazs3 <kbalazs93@gmail.com>
2021-09-19 19:38:30 +0200

    ask_difficulty function is ready, refactoring, UI improvements, game_logic extended

--- Hangman.py
+++ Hangman.py
@@ -4,11 +4,15 @@ import time
 
 def start_menu():
     print("START MENU\n\nStart Game\nQuit")
-    start_menu_command = input("Enter 's' to start game and 'q' to quit game").upper()
+    start_menu_command = input("Enter 's' to start game and 'q' to quit game\n: ").upper()
     if start_menu_command == 'S':
         main_game_logic()
     elif start_menu_command == 'Q':
+        print("\n\n\n\t\t\t\t\t\t\t\tBye-bye!\n\n\n")
         quit(0)
+    else:
+        print("\nEnter 's' or 'q'!\n")
+        start_menu()
 
 
 def ask_player_name():
@@ -17,26 +21,36 @@ def ask_player_name():
 
 
 def get_word_for_easy_and_medium_level(selected_level):
-    with open("countries-and-capitals.txt", "r") as countries_and_capitals:
-        word_list = []
-        open_file_for_read = open("countries-and-capitals.txt", "r")
-        for line in countries_and_capitals.readlines():
-            if selected_level == "easy":
-                word_list.append(line.split('|')[0].strip())
-            elif selected_level == "hard":
-                word_list.append(line.split('|')[1].strip())
-            return word_list[random.randint(0, len(word_list) - 1)]
+    word_list = []
+    open_file_for_read = open("countries-and-capitals.txt", "r")
+    for line in open_file_for_read.readlines():
+        if selected_level == "E":
+            word_list.append(line.split('|')[0].strip())
+        elif selected_level == "M":
+            word_list.append(line.split('|')[1].strip())
+    open_file_for_read.close()
+    return word_list[random.randint(0, len(word_list) - 1)]
 
 
 def get_word_to_hard_mode():
     with open("words.txt", 'r') as words:
         words_red = words.read()
         words_list = words_red.split(" ")
-        return words_list[random.randint(0, len(words_list) -1)]
+        return words_list[random.randint(0, len(words_list) - 1)]
 
 
 def print_result(winner):
     print(winner + "You have won!")
+
+
+def ask_difficulty_level():
+    difficulty_level = input("Choose difficulty level: \nEnter\n\t'e' for Easy\n\
+    t'm' for Medium\n\t'h' for Hard\n: ").upper()
+    while difficulty_level not in ['E', 'M', 'H']:
+        print("Please choose from the options! ")
+        difficulty_level = input("Choose difficulty level: \n\tEnter\n'e' for Easy\n\t'm' for Medium\n"
+                                 "\t'h' for Hard\n: ").lower()
+    return difficulty_level
 
 
 def hangman_display(lives):
@@ -131,10 +145,18 @@ def hangman_display(lives):
 def main_game_logic():
     player_name = ask_player_name()
     players_lives = 7
-    while players_lives > 0:
-        pass
+    selected_level = ask_difficulty_level()
+    word_to_find_out = ''
+    if selected_level == 'E' or selected_level == 'M':
+        word_to_find_out = get_word_for_easy_and_medium_level(selected_level)
+    elif selected_level == 'H':
+        word_to_find_out = get_word_to_hard_mode()
+    word_letters = list(word_to_find_out)
+    word_length = "_" * len(word_to_find_out)
+    guessed_letters = []
+    guessed = False
+    print(hangman_display(players_lives))
 
 
 if __name__ == '__main__':
     start_menu()
-    main_game_logic()
